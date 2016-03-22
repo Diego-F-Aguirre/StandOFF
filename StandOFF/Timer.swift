149c4d93974cb10fa75b47d4499938111292d119
Diego-F-Aguirre <diego3884@gmail.com>
2016-03-22 16:03:41 -0600

    Timer in Dashboard working

---
+++ StandOFF/Timer.swift
@@ -0,0 +1,81 @@
+//
+//  Timer.swift
+//  StandOFF
+//
+//  Created by Diego Aguirre on 3/21/16.
+//  Copyright © 2016 home. All rights reserved.
+//
+
+import UIKit
+
+class Timer: NSObject {
+
+    var seconds = NSTimeInterval(0)
+    var totalSeconds = NSTimeInterval(0)
+    private var timer: NSTimer?
+    var timerIsOn: Bool {
+        get {
+            if timer != nil {
+                return true
+            } else {
+                return false
+            }
+        }
+    }
+    
+    var string: String {
+        get {
+            let totalSeconds = Int(self.seconds)
+            
+            let hours = totalSeconds / 3600
+            let minutes = (totalSeconds - (hours * 3600)) / 60
+            let seconds = totalSeconds - (hours * 3600) - (minutes * 60)
+            
+            var hoursString = ""
+            if hours > 0 {
+                hoursString = "\(hours):"
+            }
+            
+            var minutesString = ""
+            if minutes < 10 {
+                minutesString = "0\(minutes):"
+            } else {
+                minutesString = "\(minutes):"
+            }
+            
+            var secondsString = ""
+            if seconds < 10 {
+                secondsString = "0\(seconds)"
+            } else {
+                secondsString = "\(seconds)"
+            }
+            
+            return hoursString + minutesString + secondsString
+        }
+    }
+    
+    func setTimer(seconds: NSTimeInterval, totalSeconds: NSTimeInterval) {
+        self.seconds = seconds
+        self.totalSeconds = totalSeconds
+    }
+    
+    func startTimer() {
+        if !timerIsOn {
+            timer = NSTimer.scheduledTimerWithTimeInterval(1.0, target: self, selector: #selector(Timer.updateTimer), userInfo: nil, repeats: true)
+            
+        }
+    }
+    
+    
+    func stopTimer() {
+        if timerIsOn {
+            timer?.invalidate()
+            timer = nil
+        }
+    }
+    
+    func updateTimer() {
+        seconds += 1
+        NSNotificationCenter.defaultCenter().postNotificationName("secondTick", object: self)
+    }
+}
